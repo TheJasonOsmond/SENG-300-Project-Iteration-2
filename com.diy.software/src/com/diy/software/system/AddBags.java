736f32376cd3d7473d62fdb47d8caae1e7ae02c8
Cocorico101 <83224644+Cocorico101@users.noreply.github.com>
2022-11-20 15:20:29 -0700

    Implement Purchase Store Bags in AddBags

--- com.diy.software/src/com/diy/software/system/AddBags.java
+++ com.diy.software/src/com/diy/software/system/AddBags.java
@@ -1,22 +1,13 @@
 package com.diy.software.system;
 
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.WindowConstants;
-
-public class AddBags {
+public class AddBags implements ActionListener {
 	JFrame bagFrame;
 	JPanel payPanel;
 	JDialog addOwnBagDialog;
@@ -27,6 +18,9 @@ public class AddBags {
 	private boolean payWasSuccessful = false;
 	private JButton btnCloseWindow;
 	private JButton addOwnBags;
+	int bag_purchased;
+	double bag_weight = 0.10;
+	double bag_price = 0.10;
 
 	public AddBags(DIYSystem sys) {
 		station = sys;
@@ -46,6 +40,7 @@ public class AddBags {
 		bagFrame.getContentPane().add(payPanel);
 		confirmLabel = new JLabel("");
 		confirmLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		//Button bag_quantity;
 
 		bagLabel = new JLabel("Please choose an option:");
 		bagLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -53,9 +48,41 @@ public class AddBags {
 
 		purchaseBag = new JButton("Purchase Store Bags");
 			purchaseBag.addActionListener(e -> {
-				//TODO Implementation of this.
-				
-				
+				JOptionPane select_bag_amount = new JOptionPane();
+				bag_purchased = Integer.parseInt(JOptionPane.showInputDialog(select_bag_amount,"Enter amount"));
+				//bag_purchased = amount;
+				//JOptionPane bag_amount = new JOptionPane();
+				//select_bag_amount.setVisible(true);
+				//Button bag_quantity = new Button("Select the amount");
+				//bag_amount.add(bag_quantity);
+				//bag_quantity.addActionListener(this);
+				JOptionPane confirm_bag_amount = new JOptionPane("Please press \"OK\" to confirm the amount of bags " +
+						"purchased.",
+				//JOptionPane pane = new JOptionPane("Please press \"OK\" to confirm you have grabbed the store bag
+				// in the bagging area.",
+						JOptionPane.INFORMATION_MESSAGE,
+						JOptionPane.OK_CANCEL_OPTION);
+				JDialog dialog = confirm_bag_amount.createDialog(null, "Confirm");
+				dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+				dialog.setVisible(true);
+
+				int value = ((Integer)confirm_bag_amount.getValue()).intValue();
+				if (value == JOptionPane.OK_OPTION) {
+					station.notifyBagWeightChange("Bags have been added by customer");
+					System.out.println("Bags have been added by customer");
+				} else if (value == JOptionPane.CANCEL_OPTION) {
+					System.out.println("Operation Canceled.");
+				}
+
+				//Update expected weight and price
+				if (bag_purchased > 0){
+					double new_item_weight;
+					new_item_weight = bag_purchased * bag_weight;
+					double total_bag_price;
+					total_bag_price = bag_purchased * bag_price;
+					station.updateExpectedWeight(new_item_weight);
+					station.updateGUIItemList("Store bags purchased", total_bag_price, new_item_weight);
+				}
 			});
 
 		payPanel.add(purchaseBag);
@@ -127,5 +154,13 @@ public class AddBags {
 			bagFrame.dispose();
 		else
 			System.exit(0);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//bag_purchased = 0;
+		bag_purchased++;
+		purchaseBag.setText("The number of bags purchased is" + bag_purchased);
+
 	}
 }
