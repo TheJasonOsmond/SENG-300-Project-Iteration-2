6dfaae199f9e4673f62c8aa79d5081dc5f3d7707
Dayee <dayee.lee1@ucalgary.ca>
2022-11-21 21:26:02 -0700

    import for ReceiptPrinterListener

--- com.diy.software/src/com/diy/software/system/ReceiptPrinterObserver.java
+++ com.diy.software/src/com/diy/software/system/ReceiptPrinterObserver.java
@@ -1,5 +1,10 @@
 package com.diy.software.system;
+import java.io.IOException;
+import com.jimmyselectronics.abagnale.ReceiptPrinterD;
+import com.jimmyselectronics.abagnale.ReceiptPrinterListener;
 
 public class ReceiptPrinterObserver {
     // Access the low ink and low paper
+
+
 }
