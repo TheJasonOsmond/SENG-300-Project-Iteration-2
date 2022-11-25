96019db66a8b7fc82eeb07c20bf21619a89db9b6
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2022-11-24 20:40:40 -0700

    few tests for paybyCash
    added few tests for pay by cash

--- com.diy.software.tests/src/com/diy/software/tests/PayByCashTests.java
+++ com.diy.software.tests/src/com/diy/software/tests/PayByCashTests.java
@@ -8,70 +8,218 @@ import com.diy.software.system.CustomerData;
 import com.diy.software.system.DIYSystem;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import java.util.Currency;
+import java.util.Locale;
 
 
 public class PayByCashTests {
 
-	private static final String CORRECT_PIN = "0000";
 	private DIYSystem testSystem;
 	private CustomerData testCustomerData;
 	//private DoItYourselfStation selfCheckout;
 	private DoItYourselfStationAR selfCheckout;
 	private AttendantStation attendantStation;
 	private CardReaderObserver cardReaderObs;
-	
-	
-	
+
+
+
 	@Before
 	public void setUp() throws Exception {
-		
+
 		testCustomerData = new CustomerData();
 		testSystem = new DIYSystem(testCustomerData, attendantStation);
 
 		//selfCheckout = new DoItYourselfStation();
 		selfCheckout = new DoItYourselfStationAR();
 		selfCheckout.plugIn();
-		selfCheckout.turnOn();		
-			
-		// initiate pay by credit 
+		selfCheckout.turnOn();
+
+		// initiate pay by credit
 		testSystem.resetReceiptPrice();
-		testSystem.changeReceiptPrice(1.0);
+		testSystem.changeReceiptPrice(100.0);
 
-		
+		Currency curr = Currency.getInstance(Locale.CANADA);
+
+		testSystem.payByCashStart();
+		//opens the pay by cash window
 	}
 
-	
 
-	@Test 
+
+	@Test
 	public void successfulPayment() {
-		
-	}
-	
-	
-	
 
-	@Test (expected = SimulationException.class)
+	}
+
+
+
+
+	//@Test (expected = SimulationException.class)
 	public void nothingOwed() {
 		// should not be able to reach state where payment executes and amount owing is 0
 		testSystem.changeReceiptPrice(0.00);
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
+		//testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
 	}
 
 	@Test
-	public void correctChangeReturned() {
+	/**
+	 * Pay for $0.05 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 */
+	public void successfullyPartialPayments_5c()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[4];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.05;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
 
 	}
 
+	/**
+	 * Pay for $0.10 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 */
 	@Test
-	public void partialPayments() {
+	public void successfullyPartialPayments_10c()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[3];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.10;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
 
 	}
-	
+	/**
+	 * Pay for $0.25 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 */
+	@Test
+	public void successfullyPartialPayments_25c()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[2];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.25;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+	/**
+	 * Pay for $1.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 */
+	@Test
+	public void successfullyPartialPayments_1d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[1];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 1.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+	/**
+	 * Pay for $2.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 */
+	@Test
+	public void successfullyPartialPayments_2d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[0];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 2.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+
+	/**
+	 * Paying for extra and then comparing the change returned is same as expected
+	 *
+	 */
+	@Test
+	public void correctChangeReturned()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(0.25);
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[0];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+
+		//simulate paying by giving $2
+		testSystem.InsertCoin(curr,denomination);
+
+		//we should have 1.75 as returned change
+		//simulate that is being dispensed
+		testSystem.dispenseChangeDue();
+
+		//simulating someone is collecting that change
+		double changeCollected = testSystem.collectChange();
+		//method to simulate the change is collected
+		System.out.println(changeCollected);
+		assertEquals(changeCollected, 1.75, 0.0);
 
 
-		
-	
+
+	}
+
+
+
+
+
+
 
 }
--- com.diy.software/src/com/diy/software/tests/PayByCashTests.java
+++ com.diy.software/src/com/diy/software/tests/PayByCashTests.java
@@ -6,187 +6,301 @@ import com.diy.software.system.AttendantStation;
 import com.diy.software.system.CardReaderObserver;
 import com.diy.software.system.CustomerData;
 import com.diy.software.system.DIYSystem;
+import com.unitedbankingservices.banknote.Banknote;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import java.util.Currency;
+import java.util.Locale;
 
 
 public class PayByCashTests {
 
-	private static final String CORRECT_PIN = "0000";
 	private DIYSystem testSystem;
 	private CustomerData testCustomerData;
 	//private DoItYourselfStation selfCheckout;
 	private DoItYourselfStationAR selfCheckout;
 	private AttendantStation attendantStation;
 	private CardReaderObserver cardReaderObs;
-	
-	
-	
+
+
+
 	@Before
 	public void setUp() throws Exception {
-		
+
 		testCustomerData = new CustomerData();
 		testSystem = new DIYSystem(testCustomerData, attendantStation);
-		
-		// create listener
-		cardReaderObs = new CardReaderObserver(testSystem);
 
 		//selfCheckout = new DoItYourselfStation();
 		selfCheckout = new DoItYourselfStationAR();
 		selfCheckout.plugIn();
-		selfCheckout.turnOn();		
-			
-		// initiate pay by credit 
+		selfCheckout.turnOn();
+
+		// initiate pay by credit
 		testSystem.resetReceiptPrice();
-		testSystem.changeReceiptPrice(1.0);
-		testSystem.payByCashStart();		
-		
+		testSystem.changeReceiptPrice(100.0);
+
+		Currency curr = Currency.getInstance(Locale.CANADA);
+
+		testSystem.payByCashStart();
+		//opens the pay by cash window
 	}
 
-	
-	/********************************************************************** 
-	// PAY BY CASH TESTS  
-	/********************************************************************** 
 
-	/********************************************************************** 
-	 * Blank PIN Entered
-	 */
+
 	@Test
-	public void blankPIN() {
-	
-		// blank PIN should throw InvalidPINException
-		testSystem.payByCredit("",testSystem.getReceiptPrice());
-		
-		// assert total still owed and payment NOT posted
-		assertEquals(1, testSystem.getReceiptPrice(), .1);
-		assertFalse(testSystem.getWasPaymentPosted());
-	}
-
-	
-	
-	/********************************************************************** 
-	 * test entering wrong pin sends correct messages, no update to totals
-	 */
-	@Test 
-	public void wrongPINBlockedPIN() {
-	
-		// add amount to total
-		// attempt to pay with wrong pin 
-		testSystem.payByCredit("8675", testSystem.getReceiptPrice());
-		// insure total amount owed is unchanged, correct error is set
-		assertEquals(1, testSystem.getReceiptPrice(), .1);
-		assertFalse(testSystem.getWasPaymentPosted());
-				
-		// update total
-		testSystem.changeReceiptPrice(1.00);
-		// attempt to pay with wrong pin 
-		testSystem.payByCredit("3009", testSystem.getReceiptPrice());
-		// insure total amount owed is unchanged
-		assertEquals(2, testSystem.getReceiptPrice(), .1);
-		assertFalse(testSystem.getWasPaymentPosted());
-		
-		// update total
-		// attempt to pay with wrong pin 
-		testSystem.changeReceiptPrice(1.00);
-		testSystem.payByCredit("2112", testSystem.getReceiptPrice());
-		// insure total amount owed is unchanged
-		assertEquals(3, testSystem.getReceiptPrice(), .1);
-		assertFalse(testSystem.getWasPaymentPosted());				
-		
-		// update total
-		testSystem.changeReceiptPrice(1.00);
-		// check with valid pin final time, but card should be locked (ie no payment)
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());	
-		// insure total amount owed is unchanged even with 
-		assertEquals(4, testSystem.getReceiptPrice(), .1);
-		assertFalse(testSystem.getWasPaymentPosted());
-	}	
-	
-	
-	
-	/**********************************************************************
-	 * complete a successful payment
-	 */
-	@Test 
 	public void successfulPayment() {
 
-		// make valid payment
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
-		
-		// check amount owing is 0, and successful payment posted
-		assertEquals(0, testSystem.getReceiptPrice(), .1);
-		assertTrue(testSystem.getWasPaymentPosted());
-
-		
-		// Try another transaction
-		// increase total again
-		testSystem.resetReceiptPrice();
-		testSystem.changeReceiptPrice(42.0);
-			
-		// make anther valid payment
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
-		
-		// check amount owing is 0, and successful payment posted
-		assertEquals(0, testSystem.getReceiptPrice(), .1);
-		assertTrue(testSystem.getWasPaymentPosted());
-		
 	}
-	
-	
-	
-	/**********************************************************************
-	 * Attempt to pay for nothing 
-	 */
-	@Test (expected = SimulationException.class)
+
+
+
+
+	//@Test (expected = SimulationException.class)
 	public void nothingOwed() {
-	
 		// should not be able to reach state where payment executes and amount owing is 0
 		testSystem.changeReceiptPrice(0.00);
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
+		//testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
 	}
-	
-	
 
-	
-	/**********************************************************************
-	 * Attempt to pay for an amount greater than the credit available on card
+	@Test
+	/**
+	 * Pay for $0.05 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
 	 */
-	@Test 
-	public void noCreditOnCard() {
-	
-		// set amount over limit
+	public void successfullyPartialPayments_5c()
+	{
 		testSystem.resetReceiptPrice();
-		testSystem.changeReceiptPrice(2001.00);
-				
-		// attempt to pay
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
-		
-		// ensure the amount is still the same
-		assertEquals(2001, testSystem.getReceiptPrice(), .1);		
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.05 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[4];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.05;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
 	}
-		
-	
-	/**********************************************************************
-	 * Try and pay after reaching maximum bank hold
+
+	/**
+	 * Pay for $0.10 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
 	 */
-	@Test 
-	public void bankHoldReached() {
-			
-		// complete 11 transactions to reach maximum holds for VISA
-		for (int i = 0; i < 11; i++) {
-			
-			// set up successful transaction
-			testSystem.resetReceiptPrice();
-			testSystem.changeReceiptPrice(1.0);
-			testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
-			//assertTrue(testSystem.getWasPaymentPosted());
-		}
-		
+	@Test
+	public void successfullyPartialPayments_10c()
+	{
 		testSystem.resetReceiptPrice();
-		testSystem.changeReceiptPrice(1.0);
-		testSystem.payByCredit(CORRECT_PIN, testSystem.getReceiptPrice());
-		assertFalse(testSystem.getWasPaymentPosted());
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.10 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[3];
+		//this coin is 0.10  (10 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.10;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
 	}
+	/**
+	 * Pay for $0.25 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
+	 */
+	@Test
+	public void successfullyPartialPayments_25c()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $0.25 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[2];
+		//this coin is 0.25  (25 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 0.25;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+	/**
+	 * Pay for $1.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
+	 */
+	@Test
+	public void successfullyPartialPayments_1d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $1.0 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[1];
+		//this coin is 1.0  (1.0 Dollars)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 1.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+	/**
+	 * Pay for $2.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
+	 */
+	@Test
+	public void successfullyPartialPayments_2d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $2.0 button is pressed
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[0];
+		//this coin is 2.0 ($2)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertCoin(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 2.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+
+	/**
+	 * Paying for extra and then comparing the change returned is same as expected
+	 *@author-name : Simrat Benipal
+	 */
+	@Test
+	public void correctChangeReturned()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(0.25);
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		long denomination = DIYSystem.acceptedCoinDenominations[0];
+		//this coin is 0.05  (5 Cents)
+		//public static final long[] acceptedCoinDenominations = {200l, 100l, 25l, 10l, 5l}
+
+		//simulate paying by giving $2
+		testSystem.InsertCoin(curr,denomination);
+
+		//we should have 1.75 as returned change
+		//simulate that is being dispensed
+		testSystem.dispenseChangeDue();
+
+		//simulating someone is collecting that change
+		double changeCollected = testSystem.collectChange();
+		//method to simulate the change is collected
+		System.out.println(changeCollected);
+		assertEquals(changeCollected, 1.75, 0.0);
+
+
+
+	}
+
+	/**
+	 * Pay for $5.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
+	 */
+	@Test
+	public void successfullyPartialPayments_5d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		//assuming $5 note is inserted
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		int denomination = DIYSystem.acceptedNoteDenominations[4];
+		//public static final int[] acceptedNoteDenominations = {100, 50, 20, 10, 5}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertBanknote(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 5.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+
+	/**
+	 * Pay for $100.0 out of the total payment, and if that is successful, the compare the remaining amount with expected
+	 * @author-name : Simrat Benipal
+	 */
+	@Test
+	public void successfullyPartialPayments_100d()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(100);
+		//assuming $5 note is inserted
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		int denomination = DIYSystem.acceptedNoteDenominations[0];
+		//public static final int[] acceptedNoteDenominations = {100, 50, 20, 10, 5}
+		double initialAmount = testSystem.getReceiptPrice();
+		//System.out.println("current amount to pay: " + initialAmount);
+		testSystem.InsertBanknote(curr,denomination);
+		//payment was successfull
+		double amountRemaining = initialAmount - 100.0;
+		//System.out.println("current amount to pay: " + amountRemaining);
+		//System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+		assertEquals(amountRemaining,testSystem.getReceiptPrice(), 0.0 );
+		System.out.println("current amount to pay: " + testSystem.getReceiptPrice());
+
+	}
+
+	/**
+	 * Paying for extra and then comparing the change returned is same as expected
+	 *@author-name : Simrat Benipal
+	 */
+	@Test
+	public void correctChangeReturned_note()
+	{
+		testSystem.resetReceiptPrice();
+		testSystem.changeReceiptPrice(10);
+		Currency curr = Currency.getInstance(Locale.CANADA);
+		int denomination = DIYSystem.acceptedNoteDenominations[2];
+		//public static final int[] acceptedNoteDenominations = {100, 50, 20, 10, 5}
+
+		//simulate paying by giving $2
+		testSystem.InsertBanknote(curr,denomination);
+
+		testSystem.dispenseChangeDue();
+
+		//simulating someone is collecting that change
+		double changeCollected = testSystem.collectChange();
+		//method to simulate the change is collected
+		System.out.println(changeCollected);
+		assertEquals(changeCollected, 1.75, 0.0);
+
+
+
+	}
+
+
+
+
 }
