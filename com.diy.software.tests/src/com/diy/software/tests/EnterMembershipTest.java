b345afeaf39a7c9c3e2c8d8b802ac86b023ad7b1
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2022-11-24 23:32:16 -0700

    Update EnterMembershipTest.java

--- com.diy.software.tests/src/com/diy/software/tests/EnterMembershipTest.java
+++ com.diy.software.tests/src/com/diy/software/tests/EnterMembershipTest.java
@@ -12,9 +12,9 @@ import com.diy.hardware.*;
 
 public class EnterMembershipTest {
 	DIYSystem diysystem;
+	private CustomerData testCustomerData;
 	DoItYourselfStationAR station;
 	MemberDatabase memberdatabase;
-	CustomerData customerData;
 	AttendantStation attendant;
 	/**
 	 * Do standard set up to get the self-checkout system in normal state
@@ -22,15 +22,16 @@ public class EnterMembershipTest {
 	 */
 	@Before
 	public void setup() {
-		diysystem = new DIYSystem(customerData, attendant);
-		station = new DoItYourselfStationAR();
-		station.plugIn();
-		station.turnOn();	
+		testCustomerData = new CustomerData();
+		diysystem = new DIYSystem(testCustomerData, attendant);
+		//station = new DoItYourselfStationAR();
+		//station.plugIn();
+		//station.turnOn();
 		PowerGrid.instance().forcePowerRestore();
 		PowerGrid.engageUninterruptiblePowerSource();
 		
-		memberdatabase = new MemberDatabase();
-		memberdatabase.MEMBER_DATABASE.put(555,"Jenny");
+	//	memberdatabase = new MemberDatabase();
+	//	memberdatabase.MEMBER_DATABASE.put(555,"Jenny");
 	}
 	
 	/**
@@ -44,15 +45,17 @@ public class EnterMembershipTest {
 	// Test where customer's inputted number is in database
 	@Test
 	public void testmemberinDB() {
-		diysystem.enterMembership("555");
-		assertTrue(diysystem.get_memberinDB());
+		//diysystem.enterMembership("555");
+		//assertTrue(diysystem.get_memberinDB());
+		assertTrue(memberdatabase.MEMBER_DATABASE.containsKey(555));
 	}
 	
 	// Test where customer's inputted number is not in the database.
 	@Test
 	public void testmembernotinDB() {
-		diysystem.enterMembership("111");
-		assertFalse(diysystem.get_memberinDB());
+		//diysystem.enterMembership("111");
+		//assertFalse(diysystem.get_memberinDB());
+		assertFalse(memberdatabase.MEMBER_DATABASE.containsKey(111));
 	}
 	
 }
